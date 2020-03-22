bc1042129cde25e34795913614081355f08d2b5a
beritcarvalho <56369664+beritcarvalho@users.noreply.github.com>
2020-03-22 00:09:18 -0300

    teste

---
+++ ex091jogodedados.py
@@ -0,0 +1,7 @@
+#Exercício Python 091: Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um dicionário em Python. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
+
+jogador1 = {'dado': 2}
+print(jogador1)
+jogador2 = {'dado': 3}
+print(jogador2)
+
