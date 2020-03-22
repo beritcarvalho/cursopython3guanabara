bb2b7267086c297e681594c39fdcc4e0ef3737bf
beritcarvalho <56369664+beritcarvalho@users.noreply.github.com>
2020-03-22 01:04:00 -0300

    finalizado
    exercicio finalizado.
    Tive que pegar uma ajuda com o video, pois até então nao sabia ordenar dicionario.
    depois percebi que a variavel que recebeu o dicionario ordenado, tornou uma lista com tuplas.

--- ex091jogodedados.py
+++ ex091jogodedados.py
@@ -1,14 +1,24 @@
 #Exercício Python 091: Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um dicionário em Python. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
 from random import randint
+from time import sleep
+from operator import itemgetter
 
 
-jogador = dict()
+print('Valores sorteados: ')
+jogo = {'jogador1': randint(1,6),
+        'jogador2': randint(1,6),
+        'jogador3': randint(1,6),
+        'jogador4': randint(1,6),      
+        }
 
-for c in range(0,4):
-    jogador['id'] = c+1
-    jogador['dado'] = randint(1,6)
-    print(f'Jogador{jogador["id"]} tirou {jogador["dado"]}')
-    players = jogador.copy()
-    jogador.clear
+for chave,valor in jogo.items():
+    print(f'O {chave} tirou {valor}')
+    sleep(0.5)
 
-print(players)
+
+print('\nRanking dos jogadores: ')
+ranking = dict()
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+
+for c in range(0,len(ranking)):
+    print(f'{c+1}° lugar: {ranking[c][0]} com {ranking[c][1]}') 
