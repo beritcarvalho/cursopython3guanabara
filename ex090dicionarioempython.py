ec02da4ed49434dd6307ed8f2060014a4b92a745
beritcarvalho <56369664+beritcarvalho@users.noreply.github.com>
2020-03-21 23:40:02 -0300

    quasefinalizado
    testando uhauhah

--- ex090dicionarioempython.py
+++ ex090dicionarioempython.py
@@ -1,1 +1,11 @@
 #Exercício Python 090: Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário. No final, mostre o conteúdo da estrutura na tela
+aluno = dict()
+aluno['nome'] = str(input('Nome: '))
+print(aluno)
+aluno['media'] = float(input('Media: '))
+if aluno['media'] >= 7:
+    aluno['situacao'] = 'Aprovado'
+else:
+    aluno['situacao'] = 'Reprovado'
+
+print(aluno)
