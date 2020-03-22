650e4690a6d5b56b8c1a0b152790910b217f4066
beritcarvalho <56369664+beritcarvalho@users.noreply.github.com>
2020-03-22 00:26:00 -0300

    alteraçao

--- ex091jogodedados.py
+++ ex091jogodedados.py
@@ -1,7 +1,14 @@
 #Exercício Python 091: Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um dicionário em Python. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
+from random import randint
 
-jogador1 = {'dado': 2}
-print(jogador1)
-jogador2 = {'dado': 3}
-print(jogador2)
 
+jogador = dict()
+
+for c in range(0,4):
+    jogador['id'] = c+1
+    jogador['dado'] = randint(1,6)
+    print(f'Jogador{jogador["id"]} tirou {jogador["dado"]}')
+    players = jogador.copy()
+    jogador.clear
+
+print(players)
